37475b85c81c112260907476608f346cea40c840
Dwight Nodolf <dnodolf@gmail.com>
2020-04-24 22:15:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -33,8 +33,8 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do |season, holiday|
     if season == :winter
       holiday.each do |christmas, new_years|
-        if christmas == :christmas => [] << x 
-          if new_years == :new_years => [] << x
+        if christmas == :christmas[] << x 
+          if new_years == :new_years[] << x
         end
       end
     end
